e786bb49ec4f15991bdfd60c2ad0e1699331498b
Arj Shahid <arj.shahid@rwth-aachen.de>
2019-12-06 22:13:29 +0100

    TEC-22 User data classes added

---
+++ src/main/i5/las2peer/services/moodleDataProxyService/moodleData/MoodleDataPOJO/MoodleUserData.java
@@ -0,0 +1,77 @@
+package i5.las2peer.services.moodleDataProxyService.moodleData.MoodleDataPOJO;
+
+public class MoodleUserData {
+
+    private String email;
+    private String userFullName;
+    private String courseId;
+    private String quizSummary;
+    private String courseSummary;
+    private String userId;
+    private String courseName;
+    private MoodleUserGradeItem moodleUserGradeItem;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getQuizSummary() {
+        return quizSummary;
+    }
+
+    public void setQuizSummary(String quizSummary) {
+        this.quizSummary = quizSummary;
+    }
+
+    public String getCourseSummary() {
+        return courseSummary;
+    }
+
+    public void setCourseSummary(String courseSummary) {
+        this.courseSummary = courseSummary;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+
+    public MoodleUserGradeItem getMoodleUserGradeItem() {
+        return moodleUserGradeItem;
+    }
+
+    public void setMoodleUserGradeItem(MoodleUserGradeItem moodleUserGradeItem) {
+        this.moodleUserGradeItem = moodleUserGradeItem;
+    }
+}
---
+++ src/main/i5/las2peer/services/moodleDataProxyService/moodleData/MoodleDataPOJO/MoodleUserGradeItem.java
@@ -0,0 +1,203 @@
+package i5.las2peer.services.moodleDataProxyService.moodleData.MoodleDataPOJO;
+
+public class MoodleUserGradeItem {
+
+    private String itemId;
+    private String itemNumber;
+    private String weightFormatted;
+    private String itemName;
+    private String gradeDateSubmitted;
+    private String gradeRaw;
+    private String rangeFormatted;
+    private Double percentageFormatted;
+    private String outcomeId;
+    private String gradeIsHidden;
+    private String feedbackFormat;
+    private String gradeFormatted;
+    private String gradeMin;
+    private String feedback;
+    private String gradeDateGraded;
+    private String itemType;
+    private String itemModule;
+    private String weightRaw;
+    private String itemInstance;
+    private String gradeHiddenByDate;
+    private String gradeNeedsUpdate;
+    private String gradeMax;
+
+    public String getWeightFormatted() {
+        return weightFormatted;
+    }
+
+    public void setWeightFormatted(String weightFormatted) {
+        this.weightFormatted = weightFormatted;
+    }
+
+    public String getGradeDateSubmitted() {
+        return gradeDateSubmitted;
+    }
+
+    public void setGradeDateSubmitted(String gradeDateSubmitted) {
+        this.gradeDateSubmitted = gradeDateSubmitted;
+    }
+
+    public String getGradeRaw() {
+        return gradeRaw;
+    }
+
+    public void setGradeRaw(String gradeRaw) {
+        this.gradeRaw = gradeRaw;
+    }
+
+    public String getRangeFormatted() {
+        return rangeFormatted;
+    }
+
+    public void setRangeFormatted(String rangeFormatted) {
+        this.rangeFormatted = rangeFormatted;
+    }
+
+    public Double getPercentageFormatted() {
+        return percentageFormatted;
+    }
+
+    public void setPercentageFormatted(Double percentageFormatted) {
+        this.percentageFormatted = percentageFormatted;
+    }
+
+    public String getOutcomeId() {
+        return outcomeId;
+    }
+
+    public void setOutcomeId(String outcomeId) {
+        this.outcomeId = outcomeId;
+    }
+
+    public String getGradeIsHidden() {
+        return gradeIsHidden;
+    }
+
+    public void setGradeIsHidden(String gradeIsHidden) {
+        this.gradeIsHidden = gradeIsHidden;
+    }
+
+    public String getFeedbackFormat() {
+        return feedbackFormat;
+    }
+
+    public void setFeedbackFormat(String feedbackFormat) {
+        this.feedbackFormat = feedbackFormat;
+    }
+
+    public String getGradeFormatted() {
+        return gradeFormatted;
+    }
+
+    public void setGradeFormatted(String gradeFormatted) {
+        this.gradeFormatted = gradeFormatted;
+    }
+
+    public String getGradeMin() {
+        return gradeMin;
+    }
+
+    public void setGradeMin(String gradeMin) {
+        this.gradeMin = gradeMin;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getGradeDateGraded() {
+        return gradeDateGraded;
+    }
+
+    public void setGradeDateGraded(String gradeDateGraded) {
+        this.gradeDateGraded = gradeDateGraded;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public String getItemModule() {
+        return itemModule;
+    }
+
+    public void setItemModule(String itemModule) {
+        this.itemModule = itemModule;
+    }
+
+    public String getWeightRaw() {
+        return weightRaw;
+    }
+
+    public void setWeightRaw(String weightRaw) {
+        this.weightRaw = weightRaw;
+    }
+
+    public String getItemInstance() {
+        return itemInstance;
+    }
+
+    public void setItemInstance(String itemInstance) {
+        this.itemInstance = itemInstance;
+    }
+
+    public String getGradeHiddenByDate() {
+        return gradeHiddenByDate;
+    }
+
+    public void setGradeHiddenByDate(String gradeHiddenByDate) {
+        this.gradeHiddenByDate = gradeHiddenByDate;
+    }
+
+    public String getGradeNeedsUpdate() {
+        return gradeNeedsUpdate;
+    }
+
+    public void setGradeNeedsUpdate(String gradeNeedsUpdate) {
+        this.gradeNeedsUpdate = gradeNeedsUpdate;
+    }
+
+    public String getGradeMax() {
+        return gradeMax;
+    }
+
+    public void setGradeMax(String gradeMax) {
+        this.gradeMax = gradeMax;
+    }
+
+    public String getItemNumber() {
+        return itemNumber;
+    }
+
+    public void setItemNumber(String itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+}
