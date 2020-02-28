74d17577d2d99d795210a0a0f3def94d5c063e17
Alexander Neumann <neumann@dbis.rwth-aachen.de>
2020-02-28 15:21:25 +0100

    Start thread via REST Request
    Verifies if las2peer user is equal to moodle token user (email check)

--- src/main/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyService.java
+++ src/main/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyService.java
@@ -1,6 +1,7 @@
 package i5.las2peer.services.moodleDataProxyService;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -12,6 +13,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,14 +29,33 @@ import com.google.gson.Gson;
 
 import i5.las2peer.api.Context;
 import i5.las2peer.api.ManualDeployment;
-import i5.las2peer.api.Service;
 import i5.las2peer.api.logging.MonitoringEvent;
+import i5.las2peer.api.security.AnonymousAgent;
 import i5.las2peer.logging.L2pLogger;
+import i5.las2peer.restMapper.RESTService;
+import i5.las2peer.restMapper.annotations.ServicePath;
+import i5.las2peer.security.UserAgentImpl;
 import i5.las2peer.services.moodleDataProxyService.moodleData.MoodleWebServiceConnection;
 import i5.las2peer.services.moodleDataProxyService.moodleData.MoodleDataPOJO.MoodleAssignSubmission;
 import i5.las2peer.services.moodleDataProxyService.moodleData.MoodleDataPOJO.MoodleUserData;
 import i5.las2peer.services.moodleDataProxyService.moodleData.MoodleDataPOJO.MoodleUserGradeItem;
 import i5.las2peer.services.moodleDataProxyService.moodleData.xAPIStatements.xAPIStatements;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Contact;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
+
+@Api
+@SwaggerDefinition(
+		info = @Info(
+				title = "Moodle Data Proxy Service",
+				version = "1.0.0",
+				description = "A proxy for requesting data from moodle",
+				contact = @Contact(
+						name = "Alexander Tobias Neumann",
+						email = "neumann@rwth-aachen.de")))
 
 /**
  * 
@@ -36,7 +64,8 @@ import i5.las2peer.services.moodleDataProxyService.moodleData.xAPIStatements.xAP
  * 
  */
 @ManualDeployment
-public class MoodleDataProxyService extends Service {
+@ServicePath("moodle")
+public class MoodleDataProxyService extends RESTService {
 
 	private String moodleDomain;
 	private String moodleToken;
@@ -53,7 +82,7 @@ public class MoodleDataProxyService extends Service {
 	private final static L2pLogger logger = L2pLogger.getInstance(MoodleDataProxyService.class.getName());
 
 	private static Context context = null;
-	
+
 	private static String email = "";
 
 	/**
@@ -73,21 +102,21 @@ public class MoodleDataProxyService extends Service {
 		}
 
 		moodle = new MoodleWebServiceConnection(moodleToken, moodleDomain);
-		
-		if(email.equals("")) {
+
+		if (email.equals("")) {
 			try {
-			String siteInfoRaw = moodle.core_webservice_get_site_info();
-			JSONObject siteInfo = new JSONObject(siteInfoRaw);
-			int userId = siteInfo.getInt("userid");
-			String currentUserInfoRaw = moodle.core_user_get_users_by_field("id", userId);
-			JSONArray currentUserInfo = new JSONArray(currentUserInfoRaw);
-			JSONObject u = currentUserInfo.getJSONObject(0);
-			email = u.getString("email");
-			}catch(IOException e){
+				String siteInfoRaw = moodle.core_webservice_get_site_info();
+				JSONObject siteInfo = new JSONObject(siteInfoRaw);
+				int userId = siteInfo.getInt("userid");
+				String currentUserInfoRaw = moodle.core_user_get_users_by_field("id", userId);
+				JSONArray currentUserInfo = new JSONArray(currentUserInfoRaw);
+				JSONObject u = currentUserInfo.getJSONObject(0);
+				email = u.getString("email");
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		if (courseList == null || courseList.isEmpty()) {
 			try {
 				String courses = moodle.core_course_get_courses();
@@ -104,12 +133,30 @@ public class MoodleDataProxyService extends Service {
 		}
 	}
 
-	public void initMoodleProxy() {
+	@POST
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	@ApiResponses(
+			value = { @ApiResponse(
+					code = HttpURLConnection.HTTP_OK,
+					message = "Moodle connection is initiaded") })
+	@RolesAllowed("authenticated")
+	public Response initMoodleProxy() {
+		if (Context.getCurrent().getMainAgent() instanceof AnonymousAgent) {
+			return Response.status(Status.UNAUTHORIZED).entity("Authorization required.").build();
+		}
+		UserAgentImpl u = (UserAgentImpl) Context.getCurrent().getMainAgent();
+		if (!u.getEmail().equals(email)) {
+			return Response.status(Status.FORBIDDEN).entity("Access denied").build();
+		}
 		if (dataStreamThread == null) {
 			context = Context.get();
 			dataStreamThread = Executors.newSingleThreadScheduledExecutor();
 			dataStreamThread.scheduleAtFixedRate(new DataStreamThread(), 0, MOODLE_DATA_STREAM_PERIOD,
 					TimeUnit.SECONDS);
+			return Response.status(Status.OK).entity("Thread started.").build();
+		} else {
+			return Response.status(Status.BAD_REQUEST).entity("Thread already running.").build();
 		}
 	}
 
@@ -187,8 +234,9 @@ public class MoodleDataProxyService extends Service {
 									long start = quizReviewAttempt.getLong("timestart");
 									long finish = quizReviewAttempt.getLong("timefinish");
 									gItem.setDuration(finish - start);
-									context.monitorEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2, xAPIStatements
-											.createXAPIStatementGrades(moodleUserData, gItem, moodle.getDomainName()+"*"+email+"*"));
+									context.monitorEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2,
+											xAPIStatements.createXAPIStatementGrades(moodleUserData, gItem,
+													moodle.getDomainName() + "*" + email + "*"));
 								} else if (gItem.getItemtype().equals("assign")) {
 									String assignSubmissionsRaw = moodle
 											.mod_assign_get_submissions(gItem.getIteminstance());
@@ -198,8 +246,9 @@ public class MoodleDataProxyService extends Service {
 									long start = mas.getTimecreated();
 									long finish = mas.getTimemodified();
 									gItem.setDuration(finish - start);
-									context.monitorEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2, xAPIStatements
-											.createXAPIStatementGrades(moodleUserData, gItem, moodle.getDomainName()+"*"+email+"*"));
+									context.monitorEvent(MonitoringEvent.SERVICE_CUSTOM_MESSAGE_2,
+											xAPIStatements.createXAPIStatementGrades(moodleUserData, gItem,
+													moodle.getDomainName() + "*" + email + "*"));
 								}
 								System.out.println("Item " + gItem.getId() + " graded");
 							}
