df63c328ac832f0e679186a5582cc8a8b6bc4522
Michi <mic.michael@gmx.de>
2020-10-22 15:55:52 +0200

    something happend during merge, hope it's fixed now

--- src/test/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyServiceTest.java
+++ src/test/i5/las2peer/services/moodleDataProxyService/MoodleDataProxyServiceTest.java
@@ -10,10 +10,10 @@ import i5.las2peer.testing.TestSuite;
 
 public class MoodleDataProxyServiceTest {
 
-  private final MOODLE_TOKEN = "";
-  private final MOODLE_DOMAIN = "";
-  private final COURSE_ID = 0;
-  private final TIMESTAMP = 728632800;
+  private final String MOODLE_TOKEN = "";
+  private final String MOODLE_DOMAIN = "";
+  private final int COURSE_ID = 0;
+  private final long TIMESTAMP = 728632800;
 
   @Test
   public void testStatementGenerator() throws IOException {
